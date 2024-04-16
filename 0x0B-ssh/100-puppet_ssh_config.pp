34e447ded56a72adfa3c85e91869e8a3a5ef99d1
coxy-code <cosmaskimutai.ck@gmail.com>
2024-04-16 03:41:38 -0700

    config

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # using puppet to make changes to our configuration file
 
-file {'ect/ssh/ssh_config':
+file { 'ect/ssh/ssh_cofig':
 	ensure => present,
 
 content => "
@@ -11,5 +11,4 @@ content => "
 	IdentityFile ~/.ssh/school
 	PasswordAuthentication no
 	",
-
 }
