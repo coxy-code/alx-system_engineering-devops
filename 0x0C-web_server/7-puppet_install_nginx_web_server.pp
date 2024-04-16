f8bf345fa64ad0063e9e564b715156afb3536551
coxy-code <cosmaskimutai.ck@gmail.com>
2024-04-16 08:05:21 -0700

    pupppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -8,11 +8,11 @@ file { '/var/www/html/index.html':
   content => 'Hello World!',
 }
 
-file_line { 'aaaaa':
+file_line { 'installi':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  line   => 'rewrite ^/redirect_me https://www.github.com/coxy-code permanent;',
 }
 
 service { 'nginx':
