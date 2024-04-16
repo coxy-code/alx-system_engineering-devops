8dfda870252e127930c4e421c7ec2cc5e0868f55
coxy-code <cosmaskimutai.ck@gmail.com>
2024-04-16 03:43:31 -0700

    config

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,15 @@
-#!/usr/bin/env bash
-# using puppet to make changes to our configuration file
+# make changes to config file using Puppet
 
-file { 'ect/ssh/ssh_cofig':
-	ensure => present,
+include stdlib
 
-content => "
-	
-	#SSH client configuration
-	host*
-	IdentityFile ~/.ssh/school
-	PasswordAuthentication no
-	",
+file_line { 'Refuse to authenticate using a password':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+}
+
+file_line { 'Use private key':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school'
 }
